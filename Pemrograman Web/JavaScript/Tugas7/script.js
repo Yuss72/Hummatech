a2cf97e3e4f8644ceb9818b1de52338d0753932d
Yuss72 <yusufusman11111111@gmail.com>
2025-03-20 10:18:05 +0700

    Tugas JS

--- Pemrograman Web/JavaScript/Tugas7/script.js
+++ Pemrograman Web/JavaScript/Tugas7/script.js
@@ -1,5 +1,28 @@
-src ="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js";
-
 $(document).ready(function() {
+    $("#form-input").submit(function(e) {
+        e.preventDefault();
 
+        let judul = $("#input-judul").val();
+        let isi = $("#input-text").val();
+
+        $.ajax({
+            type: "POST",
+            data: JSON.stringify({
+                title: judul,
+                body: isi,
+                userId: 1
+            }),
+            url: "https://jsonplaceholder.typicode.com/posts",
+            contentType: "application/json; charset=utf-8",
+            success: function(response) {
+                console.log("Data Terkirim: ", response);
+                let temp = `<h2>${response.title}</h2>      
+                            <p>${response.body}</p>`;
+                $("#output").prepend(temp);
+
+                $("#input-judul").val("");
+                $("#input-text").val("");
+            }
+        })
+    })   
 })
